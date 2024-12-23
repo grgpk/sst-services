7355ec6917e640584d2808c18be4f0a089821eaa
Giorgi <grgpaikidze@gmail.com>
2024-12-23 13:30:02 +0400

    feat: add project structure

---
+++ src/main/java/com/sst/sst_services/SstServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sst.sst_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SstServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sst/sst_services/SstServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sst.sst_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SstServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
